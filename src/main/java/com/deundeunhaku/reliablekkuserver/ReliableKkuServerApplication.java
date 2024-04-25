a39aa595ef0310cedfffe5f309f3380711a64be2
wan2daaa <wan2daaa@gmail.com>
2024-04-26 00:01:12 +0900

    [test] sonar cloud test commit

--- src/main/java/com/deundeunhaku/reliablekkuserver/ReliableKkuServerApplication.java
+++ src/main/java/com/deundeunhaku/reliablekkuserver/ReliableKkuServerApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ReliableKkuServerApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(ReliableKkuServerApplication.class, args);
+    SpringApplication.run(ReliableKkuServerApplication.class,  args);
   }
 
 }
